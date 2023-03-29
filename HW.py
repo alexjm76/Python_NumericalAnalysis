42338395666da874a9f630bed259d6931b985a96
alexjm <alexjm31@gmail.com>
2023-03-29 09:29:45 +0900

    HW6 #1

---
+++ HW.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def calc_f(c):
+    m = 68.1
+    v = 40
+    t = 10
+    g = 9.81
+
+    return m * g / c * (1 - np.exp(-c / m * t)) - v
+
+r=0
+l=12
+u=16
+while True:
+    r = (l + u) / 2
+    if calc_f(l)*calc_f(r) < 0:
+        u = r
+    elif calc_f(l)*calc_f(r) >0:
+        l = r
+    elif abs(calc_f(r)) < 10**-6:
+        print(r)
+        break
