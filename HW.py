c7426317d06dd57fc500e82a39fa14ea1726e238
alexjm <alexjm31@gmail.com>
2023-03-29 10:19:30 +0900

    HW6 #3

--- HW.py
+++ HW.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def calc_f(c):
     m = 68.1
@@ -11,12 +12,60 @@ def calc_f(c):
 r=0
 l=12
 u=16
-while True:
-    r = u - ((calc_f(u)*(u-l))/(calc_f(u)-calc_f(l)))
-    if calc_f(l)*calc_f(r) < 0:
-        u = r
-    elif calc_f(l)*calc_f(r) >0:
-        l = r
-    elif abs(calc_f(r)) < 10**-6:
-        print(r)
-        break
+
+
+
+def Bisection(l, u, tolerance=1e-6, max_iterations=1000):
+    num = 0
+    num_r = []
+    iteration = []
+    while num < max_iterations:
+        r = (l + u) / 2
+        if abs(calc_f(r)) < tolerance:
+            break
+        elif calc_f(l) * calc_f(r) < 0:
+            u = r
+            num += 1
+            iteration.append(num)
+            num_r.append(abs(calc_f(r)))
+        else:
+            l = r
+            num += 1
+            iteration.append(num)
+            num_r.append(abs(calc_f(r)))
+    return iteration, num_r
+
+
+def False_position(l, u, tolerance=1e-6, max_iterations=1000):
+    num = 0
+    num_r = []
+    iteration = []
+    while num < max_iterations:
+        r = u - ((calc_f(u) * (u - l)) / (calc_f(u) - calc_f(l)))
+        if abs(calc_f(r)) < tolerance:
+            break
+        elif calc_f(l) * calc_f(r) < 0:
+            u = r
+            num += 1
+            iteration.append(num)
+            num_r.append(abs(calc_f(r)))
+        else:
+            l = r
+            num += 1
+            iteration.append(num)
+            num_r.append(abs(calc_f(r)))
+    return iteration, num_r
+print(Bisection(l,u))
+print(False_position(l,u))
+
+Bi_it, Bi_num =  Bisection(l,u)
+F_it, f_num = False_position(l,u)
+
+
+plt.plot(Bi_it,Bi_num,F_it,f_num,marker="o")
+plt.grid()
+plt.yscale("log")
+plt.ylabel("f(x_{r})")
+plt.xlabel("Iteration")
+plt.title("Iteration vs f(x_{r})")
+plt.show()
